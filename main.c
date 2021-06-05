09ff9e885370aa543407c8402c7330e3608d4876
fqqf <77983090+fqqf@users.noreply.github.com>
2021-06-05 19:38:49 +0300

    Optimize local variables

--- main.c
+++ main.c
@@ -8,14 +8,14 @@
 
 #define MAX_JSON_LENGTH 1024
 #define MAX_KEY_LENGTH 512
-#define MAX_MAIL_LENGTH 1024
+#define MAX_LETTER_LENGTH 1024
 
 #define MAIL_LOCATION "..\\core\\mail.json"
 
-#define MAIL_KEY_ADR(MAIL_NUM, KEY_NUM) MAIL_NUM*(KEYS*2)+(MAIL_NUM+1)+(2*(KEY_NUM+1))
-#define MAIL_ADR(MAIL_NUM) MAIL_NUM*(KEYS*2)+(MAIL_NUM+1)
-#define MAIL_KEY_ADR_(MAIL_ADR, KEY_NUM) MAIL_ADR+(2*(KEY_NUM+1))
-#define MAIL_AMOUNT(JSON_SIZE) (JSON_SIZE-1)/(KEYS*2+1)
+#define LETTER_KEY_ADR(MAIL_NUM, KEY_NUM) MAIL_NUM*(KEYS*2)+(MAIL_NUM+1)+(2*(KEY_NUM+1))
+#define LETTER_ADR(MAIL_NUM) MAIL_NUM*(KEYS*2)+(MAIL_NUM+1)
+#define LETTER_KEY_ADR2(LETTER_ADR, KEY_NUM) LETTER_ADR+(2*(KEY_NUM+1))
+#define LETTERS_AMOUNT(JSON_SIZE) (JSON_SIZE-1)/(KEYS*2+1)
 
 #define ID 0
 #define SENDER 1
@@ -24,109 +24,130 @@
 #define BODY 4
 #define REPLY_TO 5
 
-void read_json(char *string, char path[100]);
+int parse_json(char *json_, jsmn_parser parser_, jsmntok_t *tokens_, char *path_);
+int load_json();
+int read_file(char *string, char path[100]);
 
 int find_adr_by_id(int id);
-
+int find_adr_by_theme(int *dest_adrs, char *theme);
 void get_json_value(char *dest, int address);
-int find_adr_by_theme(int* dest_adrs, char* theme);
 
-char json_string[MAX_JSON_LENGTH];
-char string[1024];
-char mail[MAX_MAIL_LENGTH];
-
-int adrs[1024];
+char json[MAX_JSON_LENGTH];
 int json_size;
-int mail_amount;
-jsmn_parser p;
-jsmntok_t t[128];
+int letters_amount;
+
+jsmn_parser parser;
+jsmntok_t tokens[TOKENS];
+
+char temp_holder[1024];
 
 int main()
 {
- read_json(json_string, MAIL_LOCATION);
 
- jsmn_init(&p);
-
- json_size = jsmn_parse(&p, json_string, strlen(json_string), t, 128);
-
- if (json_size < 0)
- {
-  printf("Failed to parse JSON: %d\n", json_size);
-  exit(1);
- }
-
+ int adrs[1024];
  find_adr_by_theme(adrs, "NO_THEME");
+
+ char letter[MAX_LETTER_LENGTH];
+
  for (int i = 1; i <= adrs[0]; i++)
  {
-  get_json_value(mail, adrs[i]);
-  printf("%s\n", mail);
+  get_json_value(letter, adrs[i]);
+  printf("%s\n", letter);
  }
-
 
  return 1;
 }
 
-FILE *fptr;
-
 int find_adr_by_id(int id)
 {
- read_json(json_string, MAIL_LOCATION);
-
- mail_amount = MAIL_AMOUNT(json_size);
+ load_json();
 
  for (int i = 0, iter_id = 0; i < json_size; i++)
  {
-  get_json_value(string, MAIL_KEY_ADR(i, ID));
-  iter_id = atoi(string);
+  get_json_value(temp_holder, LETTER_KEY_ADR(i, ID));
+  iter_id = atoi(temp_holder);
 
   if (iter_id == id)
   {
-   //printf("MAIL_KEY = %d\n", MAIL_KEY_ADR(i, ID) - 2);
-   //printf("MAIL = %d\n", MAIL_ADR(i));
-   return MAIL_ADR(i);
+   //printf("MAIL_KEY = %d\n", LETTER_KEY_ADR(i, ID) - 2);
+   //printf("MAIL = %d\n", LETTER_ADR(i));
+   return LETTER_ADR(i);
   }
  }
 
  return -1;
 }
 
-char *iter_theme;
-
-int find_adr_by_theme(int* dest_adrs, char* theme)
+int find_adr_by_theme(int *dest_adrs, char *theme)
 {
- read_json(json_string, MAIL_LOCATION);
+ load_json();
 
- mail_amount = MAIL_AMOUNT(json_size);
+ char *iter_theme;
 
- for (int i = 0,j=1; i < mail_amount; i++)
+ letters_amount = LETTERS_AMOUNT(json_size);
+
+ for (int i = 0, j = 1; i < letters_amount; i++)
  {
-  get_json_value(string, MAIL_KEY_ADR(i, THEME));
-  iter_theme = string;
+  get_json_value(temp_holder, LETTER_KEY_ADR(i, THEME));
+  iter_theme = temp_holder;
 
 
   if (strstr(iter_theme, theme))
   {
-   dest_adrs[j] = MAIL_ADR(i);
+   dest_adrs[j] = LETTER_ADR(i);
    dest_adrs[0] += 1;
    j++;
   }
  }
 
- return (dest_adrs[0]!=0) ? 1 : -1;
+ return (dest_adrs[0] != 0) ? 1 : -1;
 }
 
- void read_json(char *dest, char path[100])
+int parse_json(char *json_, jsmn_parser parser_, jsmntok_t *tokens_, char *path_) // Returns json
+{
+ read_file(json_, path_);
+ jsmn_init(&parser_);
+
+ int json_size_ = jsmn_parse(&parser_, json_, strlen(json), tokens_, 128);
+
+ if (json_size_ < 0)
  {
-  fptr = fopen(path, "r");
-  fread(dest, MAX_JSON_LENGTH, 1, fptr);
-  fclose(fptr);
+  printf("Failed to parse JSON: {%d\n}", json_size_);
+  return -1;
  }
 
- void get_json_value(char *dest, int address)
- {
-  jsmntok_t key = t[address];
-  unsigned int length = key.end - key.start;
+ return json_size_;
+}
 
-  memcpy(dest, &json_string[key.start], length);
-  dest[length] = '\0';
+int read_file(char *dest, char *path)
+{
+ FILE *fptr;
+ fptr = fopen(path, "r");
+ fread(dest, MAX_JSON_LENGTH, 1, fptr);
+ fclose(fptr);
+
+ if (fptr == NULL)
+ {
+  printf("Failed to read file: {%s}\n", path);
+  return -1;
  }
+ return 1;
+}
+
+void get_json_value(char *dest, int address)
+{
+ jsmntok_t key = tokens[address];
+ unsigned int length = key.end - key.start;
+
+ memcpy(dest, &json[key.start], length);
+ dest[length] = '\0';
+}
+
+int load_json()
+{
+ json_size = parse_json(json, parser, tokens, MAIL_LOCATION);
+ letters_amount = LETTERS_AMOUNT(json_size);
+
+ if (json_size == -1) return -1;
+ return 1;
+}
