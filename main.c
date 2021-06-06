eec46b59e0dab0105b8378c86265239665d7ac4d
fqqf <77983090+fqqf@users.noreply.github.com>
2021-06-07 02:19:59 +0300

    Add option to replace removed letters in bitmap with new ones

--- main.c
+++ main.c
@@ -24,6 +24,8 @@
 #define LETTER_KEY_ADR2(LETTER_ADR, KEY_NUM) LETTER_ADR+(2*(KEY_NUM+1))
 #define LETTERS_AMOUNT(MAIL_SIZE) (MAIL_SIZE-1)/(KEYS*2+1)
 
+#define CHECK_BITMAP(BITMAP_SIZE, ARRAY_SIZE) ()
+
 #define ID 0
 #define SENDER 1
 #define RECIEVER 2
@@ -34,6 +36,9 @@
 #define SUCCESS 0
 #define ERROR -1
 
+#define BMP_ACTIVE '1'
+#define BMP_REMOVED '0'
+
 int parse_json(char *json_, jsmn_parser parser_, jsmntok_t *tokens_, char *path_);
 
 int load_bitmap(char* bitmap_);
@@ -41,47 +46,116 @@ int load_mail(char* mail_);
 
 int read_file(char *string, char path[100]);
 
-int change_bitmap(char* mail, char* bitmap, int letter_address,char value);
+int change_bmp(char* mail, char* bitmap, int letter_address, char value);
 int find_adr_by_id(char* mail, int id);
 int find_adr_by_theme(char* mail, int *dest_adrs, char *theme);
 int get_value_by_addr(char* mail, char *dest, int address);
 
+int add_letter(char* mail, char* bitmap, char* args);
+
 jsmn_parser parser;
 jsmntok_t tokens[TOKENS];
 
-char key_holder[LETTER_LENGTH];
+char value_holder[LETTER_LENGTH];
 int addr[MAX_LETTERS_AMOUNT];
+
+int bmp_size, mail_size, letters_amount;
 
 //get_info (ВЫЗЫВАЕТСЯ ВНУТРИ)
 
 int main()
 {
  //TODO : METHOD DISPLAY
-
- find_adr_by_theme(NULL,addr, "Hey");
-
+ char* d;
+ add_letter(NULL, NULL, d);
  return 1;
 }
 
-int add_letter(char* args)
+int is_bmp_valid()
 {
-
+ if (bmp_size!=letters_amount)
+ {
+  printf("Failed to commit operation, because bitmap size!=letters amount");
+  return 0;
+ }
+ return 1;
 }
 
-int change_bitmap(char* mail, char* bitmap, int letter_address,char value)
+void replace_substr(char *origin, char *substr, char *new_substr)
+{
+ int origin_len, substr_len, new_substr_len;
+ int i, j, k;
+ int flag = 0, start, end;
+
+ origin_len = strlen(origin);
+ substr_len = strlen(substr);
+ new_substr_len = strlen(new_substr);
+
+ for (i = 0; i < origin_len; i++)
+ {
+  flag = 0;
+  start = i;
+  for (j = 0; origin[i] == substr[j]; j++, i++) if (j == substr_len - 1) flag = 1;
+  end = i;
+  if (flag == 0) {i -= j; continue;}
+
+  for (j = start; j < end; j++)
+  {
+   for (k = start; k < origin_len; k++) origin[k] = origin[k + 1];
+   origin_len -= 1;
+   i -= 1;
+  }
+
+  for (j = start; j < start + new_substr_len; j++)
+  {
+   for (k = origin_len; k >= j; k--) origin[k + 1] = origin[k];
+   origin[j] = new_substr[j - start];
+   origin_len += 1;
+   i += 1;
+  }
+ }
+}
+
+int add_letter(char* mail, char* bitmap, char* args)
 {
  char* mail_ = (mail==NULL) ? alloca(MAX_MAIL_LENGTH* sizeof(char)) : mail;
  char* bitmap_ = (bitmap==NULL) ? alloca(MAX_BITMAP_LENGTH* sizeof(char)) : bitmap;
 
- int bitmap_size = load_bitmap(bitmap_);
- int mail_size = load_mail(mail_);
- int letters_amount = LETTERS_AMOUNT(mail_size);
+ bmp_size = load_bitmap(bitmap_);
+ mail_size = load_mail(mail_);
+ letters_amount = LETTERS_AMOUNT(mail_size);
 
- if (bitmap_size!=letters_amount)
+ if (!is_bmp_valid()) return ERROR;
+
+ int free_index = -1;
+ for (int i=0; i<bmp_size; i++) if (bitmap_[i]==BMP_REMOVED) {free_index=i; break;}
+
+ if (free_index==-1)
  {
-  printf("Failed to change bitmap, because bitmap size!=letters amount");
-  return ERROR;
+
+ } else
+ {
+  int adr;
+  adr = find_adr_by_id(mail_,free_index);
+  get_value_by_addr(mail_, value_holder,adr);
+  printf("%s",value_holder);
+
+  replace_substr(mail_,value_holder,"{\"id\":replaced}");
+  printf("\n\n\n\n--------------------\n\n\n\n%s",mail_);
  }
+}
+
+int change_bmp(char* mail, char* bitmap, int letter_address, char value)
+{
+ char* mail_ = (mail==NULL) ? alloca(MAX_MAIL_LENGTH* sizeof(char)) : mail;
+ char* bitmap_ = (bitmap==NULL) ? alloca(MAX_BITMAP_LENGTH* sizeof(char)) : bitmap;
+
+ bmp_size = load_bitmap(bitmap_);
+ mail_size = load_mail(mail_);
+ letters_amount = LETTERS_AMOUNT(mail_size);
+
+ if (!is_bmp_valid()) return ERROR;
+
  bitmap_[letter_address] = value;
 
  FILE *fp = fopen(BITMAP_LOCATION, "w");
@@ -101,13 +175,13 @@ int change_bitmap(char* mail, char* bitmap, int letter_address,char value)
 int find_adr_by_id(char* mail, int id_)
 {
  char* mail_ = (mail==NULL) ? alloca(MAX_MAIL_LENGTH* sizeof(char)) : mail;
- int mail_size = load_mail(mail_);
- int letters_amount = LETTERS_AMOUNT(mail_size);
+ mail_size = load_mail(mail_);
+ letters_amount = LETTERS_AMOUNT(mail_size);
 
  for (int i = 0, iter_id_ = 0; i < letters_amount; i++)
  {
-  get_value_by_addr(mail_, key_holder, LETTER_KEY_ADR(i, ID));
-  iter_id_ = atoi(key_holder); // TODO: use strtool instead
+  get_value_by_addr(mail_, value_holder, LETTER_KEY_ADR(i, ID));
+  iter_id_ = atoi(value_holder); // TODO: use strtool instead
 
   if (iter_id_ == id_) return LETTER_ADR(i);
   //printf("MAIL_KEY = %d\n", LETTER_KEY_ADR(i, ID) - 2);
@@ -130,8 +204,8 @@ int find_adr_by_theme(char* mail, int *dest_adrs, char *theme)
 
  for (int i = 0, j = 1; i < letters_amount; i++)
  {
-  get_value_by_addr(mail_, key_holder, LETTER_KEY_ADR(i, THEME));
-  iter_theme = key_holder;
+  get_value_by_addr(mail_, value_holder, LETTER_KEY_ADR(i, THEME));
+  iter_theme = value_holder;
 
 
   if (strstr(iter_theme, theme))
@@ -143,8 +217,8 @@ int find_adr_by_theme(char* mail, int *dest_adrs, char *theme)
 
   for (int f = 1; f <= addr[0]; f++)
   {
-   get_value_by_addr(mail_, key_holder, addr[f]);
-   printf("%s\n", key_holder);
+   get_value_by_addr(mail_, value_holder, addr[f]);
+   printf("%s\n", value_holder);
   }
  }
 
